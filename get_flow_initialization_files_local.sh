fe1dd68cc7504c7e694c71d3890bbce34be2351d
Shruthi <ssaishruthi98@gmail.com>
2026-04-30 12:10:10 -0400

    Condensed the local scripts into one file and condensed cluster scripts for both narval and rorqual into one file.

--- get_flow_initialization_files_local.sh
+++
@@ -1,4 +0,0 @@
-# Download initialization files for decaying homogeneous isotropic turbulence from GDrive
-TARGET_DIR=tests/integration_tests_control_files/decaying_homogeneous_isotropic_turbulence/setup_files/
-gdown --folder 16yJ0mPB8EBLwexKJfNiEsPP40uzx6CHN -O ${TARGET_DIR}
-gdown --folder 1HS7oXwXvFYRCF8CAMV3T3jdR6PBekL9n -O ${TARGET_DIR}
--- get_gmsh_mesh_files_local.sh
+++
@@ -1,6 +0,0 @@
-# Download necessary test files from GDrive
-# Mesh files for integration tests
-gdown --folder 1QOYxwyo4MMZasTiRnayHCnZ4E8oMR0gL -O tests/
-
-# Mesh files for gmsh_reader unit tests
-gdown --folder 1Z89Qq940bP9WwBr1Y5YVhZSAErOM0Rg6 -O tests/unit_tests/grid/
